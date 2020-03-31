a543f5599f6186a11d351130bde352e865998b7f
Patrick <patrick.lopdrup@gmail.com>
2020-03-31 18:29:01 +0200

    har det i samme class

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import easyAI
+from copy import *
 
 USER = 1
 AI = 2
@@ -73,7 +74,8 @@ class KalahaGame(easyAI.TwoPlayersGame):
         self.players = players
         self.nplayer = 1
         self.board = [0]*15
-        self.seeds_per_house = 1
+        self.old_board = self.board
+        self.seeds_per_house = 4
         self.reset_board()
 
     def possible_moves(self):
@@ -88,6 +90,7 @@ class KalahaGame(easyAI.TwoPlayersGame):
         return possible
 
     def make_move(self, house):
+        self.old_board = self.board
         # scoop up from chosen house
         self._scoop(house)
         cur_house = house
@@ -113,7 +116,11 @@ class KalahaGame(easyAI.TwoPlayersGame):
                 if self.board[HAND] > 0:
                     self._drop_all(STORE_IDX[player])
         # extra turn if last stone lands in your store
-        if P[cur_house][OWNER] == self.nplayer:
+        # if the player does not get extra turn we save the board so we can undo moves
+        if P[cur_house][OWNER] != self.player:
+            print("her")
+            #self.old_board = self.board
+        else:
             if P[cur_house][ROLE] == STORE:
                 self.switch_player()
 
@@ -151,6 +158,13 @@ class KalahaGame(easyAI.TwoPlayersGame):
             for house in HOUSE_LIST[player]:
                 self.board[house] = self.seeds_per_house
 
+    def undo_move(self):
+        print(f"board før")
+        print(self.board)
+        self.board = self.old_board
+        print(f"board efter")
+        print(self.board)
+
     def _scoop(self, pit):
         self.board[HAND] += self.board[pit]
         self.board[pit] = 0
@@ -171,6 +185,101 @@ class KalahaGame(easyAI.TwoPlayersGame):
                 move = house
         return move
 
+    def eval(self, board):
+        # AI wins return 100
+        if game.board[STORE_IDX[AI]] > game.board[STORE_IDX[USER]]:
+            return 100
+        elif game.board[STORE_IDX[USER]] == game.board[STORE_IDX[AI]]:
+            return 50
+        else:
+            return 0
+
+    count = 0
+
+    def alpha_beta_search(self, ply):
+        m_move = -1
+        alpha = -float('inf')
+        beta = float('inf')
+        best_score = -float('inf')
+        #m_board = board
+        #old_m_board = board
+
+        legal_moves = self.possible_moves()
+        for move in legal_moves:
+            if ply == 0:
+                return self.eval(self.board)
+
+            self.make_move(move)
+            self.switch_player()
+            s = self.min_value(ply-1, alpha, beta)
+            self.undo_move()
+            print(f"main s: {s}")
+            print(f"move: {move}")
+            if s > best_score:
+                m_move = move
+                best_score = s
+
+            alpha = max(best_score, alpha)
+        print(f"best move {m_move}")
+        print(f"count: {self.count}")
+        return m_move
+
+    def max_value(self, ply, alpha, beta):
+        self.count += 1
+        if self.is_over():
+            return self.eval(self.board)
+
+        score = -float('inf')
+
+        legal_moves = self.possible_moves()
+        for m in legal_moves:
+            if ply == 0:
+                return self.eval(self.board)
+
+            self.switch_player()
+            self.make_move(m)
+            score = max(score, self.min_value(ply-1, alpha, beta))
+            self.undo_move()
+
+            print(f"max s: {score}")
+            if score >= beta:
+                return score
+
+            alpha = max(alpha, score)
+
+        return score
+
+    def min_value(self, ply, alpha, beta):
+        self.count += 1
+        if self.is_over():
+            return self.eval(self.board)
+
+        score = float('inf')
+        legal_moves = self.possible_moves()
+        for m in legal_moves:
+            if ply == 0:
+                return self.eval(self.board)
+
+            self.switch_player()
+            self.make_move(m)
+            score = min(score, self.max_value(ply-1, alpha, beta))
+            self.undo_move()
+            print(f"min s: {score}")
+
+            if score <= alpha:
+                return score
+
+            beta = min(beta, score)
+
+        return score
+
+
+class Player:
+    def __init__(self, player_num, ply=0):
+        self.num = player_num
+        self.opp = 2 - player_num + 1
+        self.ply = ply
+
 
 def winner():
     if game.board[STORE_IDX[USER]] > game.board[STORE_IDX[AI]]:
@@ -183,18 +292,22 @@ def winner():
 
 if __name__ == "__main__":
     human = KalahaHumanPlayer()
-    other_human = KalahaHumanPlayer()
-    game = KalahaGame([human, other_human])
+    #other_human = KalahaHumanPlayer()
+    m_ai = KalahaHumanPlayer()
+    #game = KalahaGame([human, other_human])
+    game = KalahaGame([human, m_ai])
+
+    SEARCH_DEPTH = 5
 
     while not game.is_over():
         game.show()
         if game.nplayer == USER:
             move = int(input("Enter move:"))
         else:
-            move = game.ai_move()
+            #move = game.ai_move()
+            move = game.alpha_beta_search(SEARCH_DEPTH)
             print(f"\nAI moves: {move}\n")
         game.play_move(move)
     # when game is over
     game.show()
-
     print(winner())
